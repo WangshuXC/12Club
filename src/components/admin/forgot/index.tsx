046b093ce3a6b2660058d29ab855a01ea3ecf177
plusm <1040748815@qq.com>
2025-11-24 23:55:14 +0800

    fix: 修复admin/forgot页面搜索结果为空时搜索框消失的Bug

--- src/components/admin/forgot/index.tsx
+++ src/components/admin/forgot/index.tsx
@@ -107,7 +107,7 @@ export const Forgot = ({ initialResetCodes, initialTotal, initialStats }: Props)
             </div>
 
             {/* 搜索 */}
-            {resetCodes && resetCodes.length > 0 && <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex flex-col md:flex-row gap-4">
                 <Input
                     fullWidth
                     isClearable
@@ -116,7 +116,7 @@ export const Forgot = ({ initialResetCodes, initialTotal, initialStats }: Props)
                     value={searchQuery}
                     onValueChange={handleSearch}
                 />
-            </div>}
+            </div>
 
             {loading ? (
                 <Loading hint="正在获取重置码数据..." />
