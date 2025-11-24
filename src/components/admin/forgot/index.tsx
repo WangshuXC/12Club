b13162f12e57155fcb82ecf3b59131e216820431
plusm <1040748815@qq.com>
2025-11-24 23:54:14 +0800

    fix: 修复admin/forgot页面无法搜索的Bug

--- src/components/admin/forgot/index.tsx
+++ src/components/admin/forgot/index.tsx
@@ -55,7 +55,7 @@ export const Forgot = ({ initialResetCodes, initialTotal, initialStats }: Props)
             ...(debouncedQuery && { search: debouncedQuery })
         })
 
-        const response = await FetchGet<{
+        const { data }: any  = await FetchGet<{
             resetCodes: ResetCode[]
             total: number
             stats: {
@@ -64,9 +64,9 @@ export const Forgot = ({ initialResetCodes, initialTotal, initialStats }: Props)
         }>(`/auth/forgot?${params}`)
 
         setLoading(false)
-        setResetCodes(response.resetCodes)
-        setTotal(response.total)
-        setStats(response.stats)
+        setResetCodes(data.resetCodes)
+        setTotal(data.total)
+        setStats(data.stats)
     }
 
     useEffect(() => {
